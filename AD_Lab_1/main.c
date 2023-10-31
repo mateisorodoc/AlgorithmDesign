d75de78ebc8f27c03a3ae1c3359b6ff52800c0ea
mateisorodoc <Sorodoc.A.Matei@student.utcluj.ro>
2023-10-31 22:25:39 +0200

    12-10-2023

--- AD_Lab_1/main.c
+++ AD_Lab_1/main.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <curses.h>
 #include <ctype.h>
-#include <string.h>
 
-//global variables declaration
-
-typedef struct nod{
+typedef struct nod {
     int key;
     struct nod *next;
 } TIP_NOD;
 
-TIP_NOD *q, *first = NULL, *last = NULL, *r;
+TIP_NOD *q, *first = NULL, *last = NULL;
 
-void create(int k){
+void create(int k) {
+
     TIP_NOD *p, *q1;
     int opt;
     int givenKey;
 
-    p = (TIP_NOD*)malloc(sizeof (TIP_NOD ));
+    p = (TIP_NOD *) malloc(sizeof(TIP_NOD));
 
     p->key = k; //give the node the key inserted by the user
 
-    if(q == NULL){ //intrare in lista vida
+    if (q == NULL) { //intrare in lista vida
         p->next = NULL;
         last = p;
         first = p;
         q = p;
     }
+
     else{
         printf("Lista nu este vida");
 
         printf("\nChoose where to insert new node:\n");
         printf("\n1. Before first node.\n2. After last node.\n3. After desired node k1.\n4. Before desired node k1\n0. EXIT\n");
-        scanf("%d", &opt);
+        scanf("%d", &opt); //NOLINT
         switch (opt) {
             case 1:
                 p->next = first;
@@ -48,22 +46,23 @@ void create(int k){
                 break;
             case 3:
                 printf("Please insert the key of the node that you want to insert before: ");
-                scanf("%d", &givenKey);
+                scanf("%d", &givenKey); //NOLINT
                 q1 = NULL;
                 q = first;
-                while ( q != NULL ) { //searching for the node with the key givenKey
-                    if ( q->key == givenKey ) break;
+                while (q != NULL) { //searching for the node with the key givenKey
+                    if (q->key == givenKey) break;
                     q1 = q;
                     q = q->next;
                 }
 
-                if( q != NULL){
+                if (q != NULL) {
                     //node with key givenKey has address q
-                    if(q == first){
+                    if (q == first) {
                         //insert after first node
                         p->next = first;
                         first = p;
                     }
+
                     else {
                         q1->next = p;
                         p->next = q;
@@ -72,19 +71,18 @@ void create(int k){
                 break;
             case 4:
                 printf("Please insert the key of the node that you want to insert after: ");
-                scanf("%d", &givenKey);
+                scanf("%d", &givenKey); //NOLINT
                 q1 = NULL;
                 q = first;
-                while ( q != NULL ) { //searching for the node with the key givenKey
-                    if ( q->key == givenKey ) break;
-                    q1 = q;
+                while (q != NULL) { //searching for the node with the key givenKey
+                    if (q->key == givenKey) break;
                     q = q->next;
                 }
 
-                if ( q != NULL ) {
+                if (q != NULL) {
                     p->next = q->next; /* node with key givenKey has address q */
                     q->next = p;
-                    if ( q == last )
+                    if (q == last)
                         last = p;
                 }
                 break;
@@ -94,62 +92,63 @@ void create(int k){
                 printf("\n ERROR!");
         }
     }
+}
 
+
+void display() {
+
+    TIP_NOD *tmp = first;
+
+    if (q == NULL) {
+        printf("The list is empty!");
     }
 
-
-void display(){
-TIP_NOD *tmp = first;
-
-if(q == NULL){
-    printf("The list is empty!");
-}
-
-else{
-    while(tmp != NULL){
-        printf("Key = %d\n", tmp->key);
-        tmp = tmp->next;
+    else {
+        while (tmp != NULL) {
+            printf("Key = %d\n", tmp->key);
+            tmp = tmp->next;
+        }
     }
 }
 
-}
-
-void delete(int givenKey){
-    TIP_NOD *p, *q1;
+void delete(int givenKey) {
+    TIP_NOD *q1;
     q1 = NULL;
     q = first;
-    while ( q != NULL ) { //searching for the node with the key givenKey
-        if ( q->key == givenKey ) break;
+    while (q != NULL) { //searching for the node with the key givenKey
+        if (q->key == givenKey) break;
         q1 = q;
         q = q->next;
     }
 
-    if ( q != NULL ) {
-        if(q == first){
+    if (q != NULL) {
+        if (q == first) {
             first = first->next;
             free(q);
-            if(first == NULL)
+            if (first == NULL)
                 last = NULL;
         }
-        else{
+
+        else {
             q1->next = q->next;
-            if(q == last) last = q1;
+            if (q == last) last = q1;
             free(q);
         }
     }
 }
 
-void saveListInFile(){
+void saveListInFile() {
+
     FILE *fout = fopen("fileOUT.txt", "w");
     TIP_NOD *tmp;
 
-    if(q == NULL){
+    if (q == NULL)
         fprintf(fout, "The list is empty!");
-    }
 
-    else{
+    else
+    {
         tmp = first;
-        while(tmp != NULL){
+        while (tmp != NULL) {
             fprintf(fout, "%d ", tmp->key);
             tmp = tmp->next;
         }
@@ -157,98 +156,24 @@ void saveListInFile(){
     fclose(fout);
 }
 
-
-//function declaration
-//void read(){
-//    int i, j;
-//    FILE *fin = fopen("fileIN.txt", "r");
-//    for(i = 0; i < n; i++)
-//        for(j = 0; j < n; j++)
-//            fscanf(fin, "%d", &a[i][j]);
-//
-//    fclose(fin);
-//}
-
-//void save(){
-//    int i, j;
-//    FILE *fout = fopen("fileOUT.txt", "w");
-//    for(i = 0; i < n; i++)
-//        for(j = 0; j < n; j++){
-//            fprintf(fout, "\t%d ", a[i][j]);
-//            printf("%d ", a[i][j]);
-//        }
-//    fprintf(fout, "\nProdusul este: %d", p);
-//    printf("\nProdus = %d", p);
-//    fclose(fout);
-//}
-
-//int validateName(char x[]){
-//    int i, k;
-//    for(i = 0; i < strlen(x); i++)
-//    {
-//        if(isdigit(x[i]) == 1){
-//            printf("ERROR");
-//            k = 0;
-//        }
-//        else
-//            k = 1;
-//    }
-//
-//    if(k == 0)
-//        return 0;
-//    else
-//        return 1;
-//
-//}
-//
-//void readName(){
-//    char x[10];
-//    int i = 0;
-//    while (1) {
-//        printf("Enter a name: ");
-//        scanf("%s", x);
-//        if (strcmp(x, "0") == 0) {
-//            printf("Finished");
-//            break;
-//        }
-//
-//        if (validateName(x) == 0)
-//            printf("Nume eronat");
-//        else {
-//            v[i] = (STUDENT *) malloc(sizeof(STUDENT));
-//            strcpy(v[i]->nume, x);
-//            v[i]->nr = i;
-//            i++;
-//        }
-//    }
-//    n = i - 1;
-//}
-
-
-
-//void compute(){
-//    int i, j;
-//    for (i = 0; i < n; ++i)
-//        for (j = 0; j < n; j++)
-//            if(i == j)
-//                p = p * a[i][j];
-//}
-
 int main() {
-int key = 0;
 
-    int optOp= -1;
+    int key = 0;
 
-    while (optOp != 0){
+    int optOp = -1;
+
+    while (optOp != 0) {
+
         printf("\nChoose what operation you want to make: \n");
         printf("1. Create\n2. Display\n3. Save\n4. Delete\n0. EXIT");
         printf("\nOption: ");
-        scanf("%d", &optOp);
+
+        scanf("%d", &optOp); //NOLINT
 
         switch (optOp) {
             case 1:
                 printf("Insert the key for the node you want to create:\n");
-                scanf("%d", &key);
+                scanf("%d", &key); //NOLINT
                 create(key);
                 break;
             case 2:
@@ -260,7 +185,7 @@ int key = 0;
                 break;
             case 4:
                 printf("Insert the key for the node you want to DELETE:\n");
-                scanf("%d", &key);
+                scanf("%d", &key); //NOLINT
                 delete(key);
                 break;
             case 0:
